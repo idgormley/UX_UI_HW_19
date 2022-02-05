db5540b300c255db65388408a3fbf59c6417dd3b
Ian <idgormley@gmail.com>
2022-02-05 13:17:14 -0600

    b

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 console.log("Your index.js file is loaded correctly")
+
+function menuButton{
+    console.log('clicked button');
+
+    let menubar = document.getElementById('menubar')
+
+    console.log(menubar)
+
+    // if (menubar.style.visibility === 'visible') {
+    //     menubar.style.visibility = 'hidden';
+    // } else if (menubar.style.visibility === 'hidden') {
+    //     menubar.style.visibility = 'visible';
+    // }
+    menubar.style.visibility='visible'? resume.style.visibility='hidden' : resume.style.visibility='visible';
+}
+
